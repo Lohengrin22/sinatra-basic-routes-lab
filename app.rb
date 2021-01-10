6b3a5515444ef24ada6d0397058ce0c7a56809c3
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2021-01-10 16:18:01 -0500

    commit tree 9ea3e014f0e80747344354947c3098e0bacbeafb

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do 
+        "My name is Jonathan"
+    end 
+
+    get '/hometown' do
+        "My hometown is Savannah, GA"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is I miss you by Blink 182"
+    end 
 end
